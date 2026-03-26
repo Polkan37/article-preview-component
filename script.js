f48e1e35f8d4ea1f60b534973976efad7497582a
Polkan37 <zaq2587413699@gmail.com>
2026-03-27 00:36:34 +0200

    fix js error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('.share__button');
 const container = document.querySelector('.share__container');
 
-button.addEventListener('click', () => {
+button && button.addEventListener('click', () => {
     container.classList.toggle('active');
     button.classList.toggle('active');
 });
